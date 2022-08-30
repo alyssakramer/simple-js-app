5575bcc58d55d79f298bae1658130a5b467d1591
alyssakramer <111538804+alyssakramer@users.noreply.github.com>
2022-08-30 15:50:20 -0700

    Add pokemonList to JS file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
- alert('Hello World');
-
- let favoriteFood = "fried rice";
- document.write(favoriteFood);
-
+let pokemonList = [
+    {
+        name: 'Bulbasar', 
+        height: 6, 
+        type: 'grass'
+    },
+    {
+        name: 'Eve',
+        height: 5, 
+        type: 'fire'
+    }
+    {
+        name: 'Nando',
+        height: 7, 
+        types: ['poison', 'flying']
+    }
+]
