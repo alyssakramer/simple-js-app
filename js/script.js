3b4b781aa0ccbd4b57bc67b697b519775f0ceec5
alyssakramer <111538804+alyssakramer@users.noreply.github.com>
2022-09-07 09:05:29 -0700

    Corrected conditional, only for largest pokemon

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let pokemonList = [
 ]
 
 for (let i = 0; i < pokemonList.length; i++) { // loop to pokemon characters 
-    if (pokemonList[i].height > 5) {
+    if (pokemonList[i].height > 6) {
         document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s huge!' + '</p>');
      } else {
        document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + '</p>');
