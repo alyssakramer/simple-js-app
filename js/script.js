3fbe01d94a949ff56ee04ad55230b165210c12ca
alyssakramer <111538804+alyssakramer@users.noreply.github.com>
2022-09-12 10:39:52 -0700

    Replaced for Loop with forEach Loop in JS file

--- js/script.js
+++ js/script.js
@@ -12,14 +12,19 @@ let pokemonList = [
     {
         name: 'Nando',
         height: 7, 
-        types: ['poison', 'flying']
+        type: ['poison', 'flying']
     }
 ]
 
-for (let i = 0; i < pokemonList.length; i++) { // loop to pokemon characters 
-    if (pokemonList[i].height > 6) {
-        document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s huge!' + '</p>');
-     } else {
-       document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + '</p>');
-     }
-    }
+// for (let i = 0; i < pokemonList.length; i++) { // loop to pokemon characters 
+   // if (pokemonList[i].height > 6) {
+   //      document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s huge!' + '</p>');
+   //  } else {
+   //    document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + '</p>');
+   //  }
+   // }
+
+   pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' tall and ' + pokemon.type + ' type ');
+   }); 
+   
