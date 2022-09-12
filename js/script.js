7dd87ffce7554c5e0fd269b21ed7b75813d9c43d
alyssakramer <111538804+alyssakramer@users.noreply.github.com>
2022-09-12 10:57:15 -0700

    Wrapped pokemonList in IIFE in JS file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
-let pokemonList = [
-    {
-        name: 'Bulbasar', 
-        height: 6, 
-        type: 'grass'
-    },
-    {
-        name: 'Eve',
-        height: 5, 
-        type: 'fire'
-    },
-    {
-        name: 'Nando',
-        height: 7, 
-        type: ['poison', 'flying']
-    }
-]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasar', 
+            height: 6, 
+            type: 'grass'
+        },
+        {
+            name: 'Eve',
+            height: 5, 
+            type: 'fire'
+        },
+        {
+            name: 'Nando',
+            height: 7, 
+            type: ['poison', 'flying']
+        }
+    ];
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+}) ();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' }]
 
 // for (let i = 0; i < pokemonList.length; i++) { // loop to pokemon characters 
    // if (pokemonList[i].height > 6) {
@@ -27,4 +41,3 @@ let pokemonList = [
    pokemonList.forEach(function(pokemon) {
     console.log(pokemon.name + ' is ' + pokemon.height + ' tall and ' + pokemon.type + ' type ');
    }); 
-   
